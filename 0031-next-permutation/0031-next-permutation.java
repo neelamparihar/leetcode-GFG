93c56e60e48d405eb4c4a0118e152c28937338ee
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2024-03-13 10:31:27 +0530

    Time: 1 ms (20.01%), Space: 43 MB (36.89%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,28 +1,33 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int rightmostpeak = -1;
+        // store the breaking point
+        int idx = -1;
 
         for(int i = n-2 ; i>=0 ; i--) {
             if(nums[i] < nums[i+1]) {
-                rightmostpeak = i;
+                idx = i;
                 break;
             }
         }
         
-        if(rightmostpeak == -1) {
-            Arrays.sort(nums);
+        // base case - no breakpoint found
+        if(idx == -1) {
+            Arrays.sort(nums, 0, n);
             return;
         }
 
-        for(int i=n-1 ; i>rightmostpeak ; i--) {
-            if(nums[i] > nums[rightmostpeak]) {
+
+        // finding the greater but closest num
+        for(int i=n-1 ; i>idx ; i--) {
+            if(nums[i] > nums[idx]) {
                 int temp = nums[i];
-                nums[i] = nums[rightmostpeak];
-                nums[rightmostpeak] = temp;
+                nums[i] = nums[idx];
+                nums[idx] = temp;
                 break;
             }
         }
-        Arrays.sort(nums, rightmostpeak+1, n);
+        // Reversing the remaining array.
+        Arrays.sort(nums, idx+1, n);
     }
 }
