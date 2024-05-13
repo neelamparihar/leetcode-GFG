5eb51179b803eb6772feab91c6907bd2817b8595
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2024-05-13 09:15:32 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (97.88%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int matrixScore(int[][] A) {
+        int m = A.length;
+        int n = A[0].length;
+        int result = 0;
+        
+        result += (1 << (n-1))*m;
+        
+        for(int j = 1;j < n;j++){
+            int same = 0;
+            for(int i = 0;i < m;i++){
+                if(A[i][0] == A[i][j])
+                    same++;
+            }
+            result += (1 << (n-1-j))*Math.max(same,m-same);
+        }
+        
+        return result;
+    }
+}
