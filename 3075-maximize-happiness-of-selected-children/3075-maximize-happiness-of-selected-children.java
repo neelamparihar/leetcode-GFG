037186559804a6d1e3fbe1196cf1713c33a9e759
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2024-07-18 17:13:26 +0530

    Time: 35 ms (52.28%), Space: 61.7 MB (50.91%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -5,13 +5,7 @@ class Solution {
         long ans = 0;
         int n = happiness.length;
         int count = 0;
-
-        // for (int i = n - 1; i >= n - k; --i) {
-        //     happiness[i] = Math.max(happiness[i] - j++, 0);
-        //     res += happiness[i];
-        // }
-        
-            
+              
             for(int i=n-1;i>=n-k;i--)
             {
                 
@@ -19,8 +13,6 @@ class Solution {
                 count++;
                 ans+=happiness[i];
             }
-            
-        
 
         return ans;
     }
