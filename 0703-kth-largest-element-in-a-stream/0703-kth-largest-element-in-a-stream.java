03fe09d0c62161ae324c9d7af076e6e75349bd98
Neelam Parihar <108782093+neelamparihar@users.noreply.github.com>
2024-08-12 10:04:45 +0530

    Time: 13 ms (82.29%), Space: 46.9 MB (61.33%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,29 @@
+class KthLargest {
+    
+    PriorityQueue<Integer> q = new PriorityQueue<>(); // Space-:O(K)
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+
+        this.k=k;
+        // Time-: O(n*logn) for intitalization
+        for(int i:nums){
+            q.offer(i);
+        }
+
+        while(q.size()>k) q.poll();
+        
+
+    }
+    
+    public int add(int val) {
+        
+        // System.out.println(q);
+        q.add(val);//Time-:O(logn)
+        while(q.size()>k)//  Nearly Constant
+        q.poll();
+        // System.out.println(q);
+        return q.peek();
+        
+    }
+}
